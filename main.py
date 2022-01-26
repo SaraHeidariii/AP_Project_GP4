f0c1c42df2aa880fc7d3b3781029d65a708c67fd
Saharnaz <saharnazeslami@gmail.com>
2022-01-26 20:28:40 +0330

    import os

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
+import os
 import sys
+
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 import random
 
-class Mainwindow(QMainWindow):
+Form = uic.loadUiType(os.path.join(os.getcwd(), "main.ui"))[0]
+
+
+class Mainwindow(QMainWindow, Form):
     def __init__(self):
-        super(Mainwindow,self).__init__()
-        loadUi("main.ui",self)
-        self.Boards = [self.board_1,self.board_2,self.board_3,self.board_4,self.board_5,self.board_6,self.board_7,self.board_8,self.board_9,self.board_10,self.board_11,self.board_12]
-        self.Results = [self.Res1,self.Res2,self.Res3,self.Res4,self.Res5,self.Res6,self.Res7,self.Res8,self.Res9,self.Res10,self.Res11,self.Res12]
+        super(Mainwindow, self).__init__()
+        loadUi("main.ui", self)
+        self.Boards = [
+            self.board_1,
+            self.board_2,
+            self.board_3,
+            self.board_4,
+            self.board_5,
+            self.board_6,
+            self.board_7,
+            self.board_8,
+            self.board_9,
+            self.board_10,
+            self.board_11,
+            self.board_12,
+        ]
+        self.Results = [
+            self.Res1,
+            self.Res2,
+            self.Res3,
+            self.Res4,
+            self.Res5,
+            self.Res6,
+            self.Res7,
+            self.Res8,
+            self.Res9,
+            self.Res10,
+            self.Res11,
+            self.Res12,
+        ]
         self.hide_object(self.Boards)
         self.hide_object(self.Results)
         self.board.hide()
@@ -18,10 +50,25 @@ class Mainwindow(QMainWindow):
         self.cover.hide()
         self.confirmbtn.hide()
         self.startbtn.clicked.connect(self.start)
-        self.hex_color = {"red" : "#ff0000" , "blue" : "#00007f" , "green" : "#005500" , "white" : "#ffffff" , "pink" : "#fcaf95" , "orange" : "#ffaa00" , "yellow" : "#ffff00" , "sky" : "#00ffff"}
-        self.goal_color = [self.goal1,self.goal2,self.goal3,self.goal4,self.goal5]
+        self.hex_color = {
+            "red": "#ff0000",
+            "blue": "#00007f",
+            "green": "#005500",
+            "white": "#ffffff",
+            "pink": "#fcaf95",
+            "orange": "#ffaa00",
+            "yellow": "#ffff00",
+            "sky": "#00ffff",
+        }
+        self.goal_color = [self.goal1, self.goal2, self.goal3, self.goal4, self.goal5]
         self.color_index = 0
-        self.mycolor = [self.my_color1,self.my_color2,self.my_color3,self.my_color4,self.my_color5]
+        self.mycolor = [
+            self.my_color1,
+            self.my_color2,
+            self.my_color3,
+            self.my_color4,
+            self.my_color5,
+        ]
         self.red.clicked.connect(lambda: self.set_color("red"))
         self.orange.clicked.connect(lambda: self.set_color("orange"))
         self.blue.clicked.connect(lambda: self.set_color("blue"))
@@ -43,22 +90,27 @@ class Mainwindow(QMainWindow):
         self.goal.show()
         self.hide_object(self.Boards)
         self.hide_object(self.Results)
-        colors = ["red" , "blue" , "green" , "white" , "pink" , "orange" , "yellow" , "sky"]
+        colors = ["red", "blue", "green", "white", "pink", "orange", "yellow", "sky"]
         self.Goal = []
         num = 7
         for i in range(5):
-            index = random.randint(0,num)
-            self.goal_color[i].setStyleSheet(f"background-color: {self.hex_color[colors[index]]};")
+            index = random.randint(0, num)
+            self.goal_color[i].setStyleSheet(
+                f"background-color: {self.hex_color[colors[index]]};"
+            )
             self.Goal.append(colors[index])
             colors.pop(index)
             num = num - 1
         self.goal.show()
 
-    def set_color(self,color):
-        self.mycolor[self.color_index].setStyleSheet(f"background-color: {self.hex_color[color]};")
+    def set_color(self, color):
+        self.mycolor[self.color_index].setStyleSheet(
+            f"background-color: {self.hex_color[color]};"
+        )
         self.color_index += 1
         if self.color_index == 5:
             self.color_index = 0
+
 
 app = QApplication(sys.argv)
 widget = QtWidgets.QStackedWidget()
