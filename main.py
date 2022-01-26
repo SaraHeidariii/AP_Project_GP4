669b4644607c2caf4d322fc74959759173e464ad
SARA <sara.blossom2000@gmail.com>
2022-01-26 23:35:47 +0330

    few changes in color & size

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication,QMainWindow
 import random
 
+
 class Mainwindow(QMainWindow):
     def __init__(self):
         super(Mainwindow,self).__init__()
         loadUi("main.ui",self)
         self.Boards = [self.board_1,self.board_2,self.board_3,self.board_4,self.board_5,self.board_6,self.board_7,self.board_8,self.board_9,self.board_10,self.board_11,self.board_12]
         self.Results = [self.Res1,self.Res2,self.Res3,self.Res4,self.Res5,self.Res6,self.Res7,self.Res8,self.Res9,self.Res10,self.Res11,self.Res12]
+        
         self.hide_object(self.Boards)
         self.hide_object(self.Results)
         self.board.hide()
@@ -17,15 +19,17 @@ class Mainwindow(QMainWindow):
         self.goal.hide()
         self.cover.hide()
         self.confirmbtn.hide()
+        
         self.startbtn.clicked.connect(self.start)
-        self.hex_color = {"red" : "#ff0000" , "blue" : "#00007f" , "green" : "#005500" , "white" : "#ffffff" , "pink" : "#fcaf95" , "orange" : "#ffaa00" , "yellow" : "#ffff00" , "sky" : "#00ffff"}
+        self.hex_color = {"red" : "#ff0000" , "blue" : "#00007f" , "green" : "#005500" , "white" : "#ffffff" , "pink" : "#fcaf95" , "orange" : "#ffaa00" , "yellow" : "#ffff00" , "purple" : "#800080"}
         self.goal_color = [self.goal1,self.goal2,self.goal3,self.goal4,self.goal5]
         self.color_index = 0
         self.mycolor = [self.my_color1,self.my_color2,self.my_color3,self.my_color4,self.my_color5]
+        
         self.red.clicked.connect(lambda: self.set_color("red"))
         self.orange.clicked.connect(lambda: self.set_color("orange"))
         self.blue.clicked.connect(lambda: self.set_color("blue"))
-        self.sky.clicked.connect(lambda: self.set_color("sky"))
+        self.purple.clicked.connect(lambda: self.set_color("purple"))
         self.pink.clicked.connect(lambda: self.set_color("pink"))
         self.white.clicked.connect(lambda: self.set_color("white"))
         self.yellow.clicked.connect(lambda: self.set_color("yellow"))
@@ -39,11 +43,13 @@ class Mainwindow(QMainWindow):
         self.confirmbtn.show()
         self.board.show()
         self.color_btn.show()
-        # self.cover.show()
+        # self.cover.show()  fln bara check krdne rangaye entekhabie comp
         self.goal.show()
+        
         self.hide_object(self.Boards)
         self.hide_object(self.Results)
-        colors = ["red" , "blue" , "green" , "white" , "pink" , "orange" , "yellow" , "sky"]
+        
+        colors = ["red" , "blue" , "green" , "white" , "pink" , "orange" , "yellow" , "purple"]
         self.Goal = []
         num = 7
         for i in range(5):
